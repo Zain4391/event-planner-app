5f29f85219154c12d2ad62c8155193986b216eb8
Zain4391 <manutdfast91@gmail.com>
2025-08-19 01:12:10 +0500

    Create Event done, update event dto add, no test

--- backend/src/event/dto/update-event-dto.ts
+++ backend/src/event/dto/update-event-dto.ts
@@ -1,48 +1,57 @@
 import { Transform } from "class-transformer";
 import { IsDateString, IsInt, IsOptional, IsString, IsUUID, Length, Min, IsUrl, Matches, MinDate } from "class-validator";
 
-export class CreateEventDto {
+export class UpdateEventDto {
+    @IsOptional()
     @IsString()
     @Length(1, 255)
-    title: string;
+    title?: string;
 
     @IsOptional()
     @IsString()
     description?: string;
 
+    @IsOptional()
     @IsUUID()
-    categoryId: string;
+    categoryId?: string;
 
+    @IsOptional()
     @IsString()
     @Length(1, 255)
-    venueName: string;
+    venueName?: string;
 
+    @IsOptional()
     @IsString()
-    venueAddress: string;
+    venueAddress?: string;
 
+    @IsOptional()
     @IsDateString()
     @Transform(({ value }) => new Date(value))
     @MinDate(new Date(), {
         message: "Event date must be in the future."
     })
-    eventDate: string;  // "2024-12-25"
+    eventDate?: string;  // "2024-12-25"
 
+    @IsOptional()
     @IsString()
     @Matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/, {
         message: 'startTime must be in HH:MM:SS format'
     })
-    startTime: string;  // "14:30:00"
+    startTime?: string;  // "14:30:00"
 
+    @IsOptional()
     @IsString()
     @Matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/, {
         message: 'startTime must be in HH:MM:SS format'
     })
-    endTime: string;    // "18:00:00"
+    endTime?: string;    // "18:00:00"
 
+    @IsOptional()
     @IsInt()
     @Min(1)
-    totalCapacity: number;
+    totalCapacity?: number;
 
+    @IsOptional()
     @IsOptional()
     @IsUrl()
     bannerImageUrl?: string;
--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import type { DatabaseType } from 'src/database/database.module';
+import * as schema from "../database/schemas/schema";
+import { ConfigService } from '@nestjs/config';
+import { and, eq } from 'drizzle-orm';
+import { CreateEventDto } from './dto/create-event-dto';
+import { UpdateEventDto } from './dto/update-event-dto';
+
 
 @Injectable()
-export class EventService {}
+export class EventService {
+
+    constructor(
+        @Inject("DATABASE_CONNECTION") private readonly db: DatabaseType,
+        private readonly configService: ConfigService
+    ) {}
+
+    async findAll(userRole?: string) {
+        try {
+            const events = await this.db.select().from(schema.events);
+            if(events.length === 0) {
+                return [];
+            }
+
+            const publishedEvents = events.map((event) => event.status === "Published");
+            return publishedEvents;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async findOne(id: string, organizerId?: string, userRole?: string) {
+        try {
+            const [event] = await this.db.select().from(schema.events).where(eq(schema.events.id, id));
+
+            if(!event) {
+                throw new NotFoundException("Event not found");
+            }
+
+            if(userRole === "Admin") {
+                return event;
+            }
+
+            if(event.status === "Published") {
+                return event;
+            }
+
+            if(organizerId && event.organizerId === organizerId) {
+                return event;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async create(createeventDto: CreateEventDto, organizerId: string) {
+        try {
+            const [category] = await this.db.select().from(schema.categories).where(eq(
+                schema.categories.id,
+                createeventDto.categoryId
+            ));
+
+            if(!category) {
+                throw new NotFoundException("Category not found");
+            }
+
+            // Validate event date is in future
+            const eventDateTime = new Date(`${createeventDto.eventDate}T${createeventDto.startTime}`);
+            if (eventDateTime <= new Date()) {
+                throw new BadRequestException('Event date must be in the future');
+            }
+
+            // Validate start time < end time
+            if (createeventDto.startTime >= createeventDto.endTime) {
+                throw new BadRequestException('Start time must be before end time');
+            }
+
+            // create the event draft
+            const [event] = await this.db.insert(schema.events).values({
+                ...createeventDto,
+                organizerId: organizerId,
+                availableCapacity: createeventDto.totalCapacity,
+                status: 'Draft'
+            }).returning()
+
+            return event;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async update(updateEventDto: UpdateEventDto, organizerId: string) {
+        /* TODO */
+    }
+
+    async confirmAndPublish(eventId: string) {
+        /* TODO */
+    }
+
+    async remove(eventId: string) {
+        /* TODO */
+    }
+}
